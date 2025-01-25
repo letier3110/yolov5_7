98a50ad0c6a73af1159ba184c1f2a89bba47a68d
letier3110 <letier3110@gmail.com>
2025-01-25 12:08:01 +0200

    Refactor detection function to use a queue for frame processing and simplify parameters

--- detect.py
+++ detect.py
@@ -31,6 +31,7 @@ import os
 import platform
 import sys
 from pathlib import Path
+import queue
 
 import torch
 
@@ -54,35 +55,35 @@ from utils.torch_utils import select_device, smart_inference_mode
 
 
 @smart_inference_mode()
-def run(
-        weights=ROOT / 'yolov5s.pt',  # model path or triton URL
-        source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
-        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
-        imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        view_img=False,  # show results
-        save_txt=False,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
-        save_crop=False,  # save cropped prediction boxes
-        nosave=False,  # do not save images/videos
-        classes=None,  # filter by class: --class 0, or --class 0 2 3
-        agnostic_nms=False,  # class-agnostic NMS
-        augment=False,  # augmented inference
-        visualize=False,  # visualize features
-        update=False,  # update all models
-        project=ROOT / 'runs/detect',  # save results to project/name
-        name='exp',  # save results to project/name
-        exist_ok=False,  # existing project/name ok, do not increment
-        line_thickness=3,  # bounding box thickness (pixels)
-        hide_labels=False,  # hide labels
-        hide_conf=False,  # hide confidences
-        half=False,  # use FP16 half-precision inference
-        dnn=False,  # use OpenCV DNN for ONNX inference
-        vid_stride=1,  # video frame-rate stride
-):
+def detect(FrameQueue: queue.Queue):
+    weights='./best.pt' # model path or triton URL
+    source='0' # file/dir/URL/glob/screen/0(webcam)
+    data='data/coco128.yaml' # dataset.yaml path
+    imgsz=(640640) # inference size (heightwidth)
+    conf_thres=0.25 # confidence threshold
+    iou_thres=0.45 # NMS IOU threshold
+    max_det=1000 # maximum detections per image
+    device='' # cuda devicei.e. 0 or 0,1,2,3 or cpu
+    view_img=False # show results
+    save_txt=False # save results to *.txt
+    save_conf=False # save confidences in --save-txt labels
+    save_crop=False # save cropped prediction boxes
+    nosave=True # do not save images/videos
+    classes=None # filter by class: --class 0or --class 0 2 3
+    agnostic_nms=False # class-agnostic NMS
+    augment=False # augmented inference
+    visualize=False # visualize features
+    update=False # update all models
+    project='runs/detect' # save results to project/name
+    name='exp' # save results to project/name
+    exist_ok=False # existing project/name okdo not increment
+    line_thickness=3 # bounding box thickness (pixels)
+    hide_labels=False # hide labels
+    hide_conf=False # hide confidences
+    half=False # use FP16 half-precision inference
+    dnn=False # use OpenCV DNN for ONNX inference
+    vid_stride=1 # video frame-rate stride
+
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
@@ -168,6 +169,7 @@ def run(
 
                 for *xyxy, conf, cls in det:
                     if int(cls) == 0:  # Check if the class is 0, a 'turret'
+                        FrameQueue.put(det[:, :4])
                         x1, y1, x2, y2 = map(int, xyxy)  # Extract coordinates
 
                         # Calculate center coordinates
@@ -193,8 +195,8 @@ def run(
 
                         # Print the center coordinates
                         print(f"Center coordinates: ({center_x:.0f}, {center_y:.0f})")
-                        ser_data = [255, high_xbyte, low_xbyte,high_ybyte, low_ybyte, angle]
-                        data = bytes(ser_data)
+                        # ser_data = [255, high_xbyte, low_xbyte,high_ybyte, low_ybyte, angle]
+                        # data = bytes(ser_data)
                         # arduino.write(data) #sending the picking cordinates to Arduino
                         # print(ser_data)
 
@@ -202,7 +204,7 @@ def run(
                     # if arduino.in_waiting > 0:  # print data from Arduino
                     #     print(arduino.readline())
 
-                names[int(c)]
+                # names[int(c)]
 
                 # names[int(c)]
                 # n
@@ -264,39 +266,39 @@ def run(
         strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
 
 
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path or triton URL')
-    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob/screen/0(webcam)')
-    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
-    opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-    print_args(vars(opt))
-    return opt
+# def parse_opt():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path or triton URL')
+#     parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob/screen/0(webcam)')
+#     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
+#     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+#     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+#     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+#     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+#     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+#     parser.add_argument('--view-img', action='store_true', help='show results')
+#     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+#     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+#     parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+#     parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+#     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+#     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+#     parser.add_argument('--augment', action='store_true', help='augmented inference')
+#     parser.add_argument('--visualize', action='store_true', help='visualize features')
+#     parser.add_argument('--update', action='store_true', help='update all models')
+#     parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
+#     parser.add_argument('--name', default='exp', help='save results to project/name')
+#     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+#     parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+#     parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+#     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+#     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+#     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+#     parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
+#     opt = parser.parse_args()
+#     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+#     print_args(vars(opt))
+#     return opt
 
 
 def main(opt):
@@ -305,5 +307,4 @@ def main(opt):
 
 
 if __name__ == "__main__":
-    opt = parse_opt()
-    main(opt)
+    main()
